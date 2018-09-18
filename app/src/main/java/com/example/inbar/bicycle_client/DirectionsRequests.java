5ef5551626ff643e9c911522f1fbfa404ee181dc
Wolfovich, Rotem <rotem.wolfovich@hpe.com>
2018-09-18 23:02:21 +0300

    fix routes

--- app/src/main/java/com/example/inbar/bicycle_client/DirectionsRequests.java
+++ app/src/main/java/com/example/inbar/bicycle_client/DirectionsRequests.java
@@ -2,6 +2,7 @@ package com.example.inbar.bicycle_client;
 
 import android.graphics.Color;
 import android.os.AsyncTask;
+import android.util.Pair;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
@@ -10,11 +11,15 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cz.msebera.android.httpclient.HttpEntity;
 import cz.msebera.android.httpclient.HttpResponse;
@@ -29,16 +34,19 @@ public class DirectionsRequests extends AsyncTask<String, Object, String> {
 
     private String _Key = "AIzaSyAK4YCms8YltWWh_beAVXDylQw-GCW55_s";
     URIBuilder _builder;// = new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/directions/json");
-    URIBuilder _builderReturnRoute=new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/directions/json");
     HttpClient _client = new DefaultHttpClient();
     ArrayList<Place> _resultList;
     GoogleMap _mMap;
-    double _Userlat ,_Userlng;
+    double _Userlat ,_Userlng;//user potion x;y
     String _retSrc;
     LatLng _stationPosition1;
+    Map<String, List <Place>> _placesDictionary;
+    int _placesTotalNumber=0;
+    List<Place> _choosenPlacesList=new ArrayList<Place>() ;
 
     public DirectionsRequests(ArrayList<Place> resultList, double lat, double lng, GoogleMap mMap, LatLng stationPosition1) {
 
+        _placesTotalNumber=resultList.size();
         _resultList=resultList;
         _Userlat=lat;
         _Userlng=lng;
@@ -53,36 +61,33 @@ public class DirectionsRequests extends AsyncTask<String, Object, String> {
         UpdateApiUrl();
 
         HttpUriRequest request = null;
-        //HttpUriRequest request2 = null;
+
         try {
             request = new HttpGet(_builder.build());
-            //request2 = new HttpGet(_builderReturnRoute.build());
+
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
 
         HttpResponse execute = null;
-       // HttpResponse execute2 = null;
+
         try {
             execute = _client.execute(request);// do the api request
-            // do the api request
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         HttpEntity response = execute.getEntity();
 
-        //    execute2 = _client.execute(request);
-      //  } catch (IOException e) {
-       //     e.printStackTrace();
-       // }
-       // HttpEntity response2 = execute.getEntity();
+
         if (response != null) {
 
-           // String retSrc2 = null;
+
             try {
                 _retSrc=retSrc = EntityUtils.toString(response);
-                //retSrc2 = EntityUtils.toString(response);
+                SaveUserRoute();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -90,6 +95,14 @@ public class DirectionsRequests extends AsyncTask<String, Object, String> {
         return null;
     }
 
+    private void SaveUserRoute() throws IOException {
+        File File = new File("cityCycleRoute");
+        FileWriter FileWriter = new FileWriter(File, false); // true to append
+        // false to overwrite.
+        FileWriter.write(_retSrc);
+
+
+    }
 
 
     @Override
@@ -104,40 +117,76 @@ public class DirectionsRequests extends AsyncTask<String, Object, String> {
 
     }
 
+    private void SortLocationDictionaryByCategory(){
+
+       _placesDictionary = new HashMap<>();
+        for (Place p:_resultList)
+        {
+            if(_placesDictionary.containsKey(p.category)){
+                List <Place> places_list= _placesDictionary.get(p.category);
+                places_list.add(p);
+
+            }
+            else
+            {
+                List <Place>places=  new ArrayList<Place>();
+                places.add(p);
+                _placesDictionary.put(p.category,places);
+            }
+        }
+    }
+
+    private  String BuildWayPointsAtrribute(){
+
+        StringBuilder wayPointStr = new StringBuilder();
+
+        SortLocationDictionaryByCategory();
+        int categoryNmber=_placesDictionary.size();
+
+        int placesInRoute=0;
+        int attemps=0;
+     while( placesInRoute<5 && attemps<5) {
+
+         for (Map.Entry<String, List<Place>> entry : _placesDictionary.entrySet() ) {
+             List<Place> places = entry.getValue();
+             Collections.sort(places, Place::compareTo);//sort by rating
+             if (placesInRoute < 5) {
+
+
+                 if( places.size() >0) {
+
+                     wayPointStr = wayPointStr.append("|" + places.get(0).lat + "," + places.get(0).lng);
+                     _choosenPlacesList.add(places.get(0));
+                     places.get(0).isInWayPoint=true;
+                     placesInRoute++;
+                     places.remove(0);
+                 }
+                 if (places.size() == 0) {
+
+                 }
+
+             }
+         }
+         attemps++;
+     }
+        return wayPointStr.toString();
+    }
+
     private void UpdateApiUrl() {
+
+
           _builder = new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/directions/json");
-        int[] indexArr;
-        List<Integer> soutePlaces = new ArrayList<Integer>();
-        List<Integer> NorthePlaces = new ArrayList<Integer>();
-        indexArr= SortThePlacesToSouthOrNorth(soutePlaces,NorthePlaces);
+
 
         _builder.addParameter("origin",_Userlat + "," + _Userlng);
-        _builder.addParameter("destination",_resultList.get(indexArr[0]).lat+","+_resultList.get(indexArr[0]).lng);// the destination is the most souhe Place
-        _builder.addParameter("mode", "WALKING");
-        _builder.addParameter("waypoints",_resultList.get(0).name);
-       _builder.addParameter("optimize","true");
-       double x=_stationPosition1.latitude;
-       //_builder.addParameter("via",_stationPosition1.latitude+","+_stationPosition1.longitude);
-        //_builder.addParameter("via",_resultList.get(soutePlaces.get(0)).lat+","+_resultList.get(soutePlaces.get(0)).lng);
-        //_builder.addParameter("avoid","highways");
+        _builder.addParameter("destination",_Userlat + "," + _Userlng );// the destination is the most souhe Place
+
+        _builder.addParameter("mode", "walking");
+        _builder.addParameter("waypoints",_stationPosition1.latitude+","+_stationPosition1.longitude+"|"+BuildWayPointsAtrribute());
+        _builder.addParameter("optimize","true");
+
+        _builder.addParameter( "avoid","highways");
         _builder.addParameter("key",this._Key);
-
-        // UpdateApiUrlSecoundPart
-       // _builderReturnRoute.addParameter("origin",_resultList.get(indexArr[0]).lat+","+_resultList.get(indexArr[0]).lng);
-       // _builderReturnRoute.addParameter("destination",_resultList.get(indexArr[1]).lat+","+_resultList.get(indexArr[1]).lng );// the destination is the most souhe Place
-       // _builderReturnRoute.addParameter("mode", "WALKING");
-       // _builderReturnRoute.addParameter("optimize","true");
-
-
-        //_builder.addParameter("via",_resultList.get(soutePlaces.get(1)).lat+","+_resultList.get(soutePlaces.get(1)).lng);
-        //_builder.addParameter("via",_resultList.get(NorthePlaces.get(0)).lat+","+_resultList.get(NorthePlaces.get(0)).lng);
-
-
-
-        //_builder.addParameter("avoid","highways");
-        _builderReturnRoute.addParameter("key",this._Key);
-
-
 
     }
 
